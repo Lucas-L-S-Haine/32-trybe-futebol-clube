e57b47d0225611ab5c516470e9fdaa368b1685f0
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-25 19:07:44 -0300

    feat: add inProgress query parameter

--- app/backend/src/controllers/match.ts
+++ app/backend/src/controllers/match.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from 'express';
 import * as service from '../services/match';
 
 export const readAll: RequestHandler = (req, res, next) => service
-  .readAll()
+  .readAll(req.query.inProgress as unknown as string | undefined)
   .then((matches) => res.status(200).json(matches))
   .catch(next);
 
--- app/backend/src/services/match.ts
+++ app/backend/src/services/match.ts
@@ -1,7 +1,9 @@
 import { Match } from '../database/models';
 
-export const readAll = async () => {
-  const matches = await Match.findAll();
+export const readAll = async (inProgress: string | undefined) => {
+  const matches = await inProgress === undefined
+    ? Match.findAll()
+    : Match.findAll({ where: { inProgress: JSON.parse(inProgress as string) } });
   return matches;
 };
 
