26dfb28b24cc7afa528b2461cafaf3e858aac8df
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-26 17:54:38 -0300

    fix: change status code and message

--- app/backend/src/services/match.ts
+++ app/backend/src/services/match.ts
@@ -2,6 +2,8 @@ import { WhereOptions } from 'sequelize';
 import { Match, Club } from '../database/models';
 import HttpException from '../classes/httpException';
 
+const EQUAL_TEAM_ERROR = 'It is not possible to create a match with two equal teams';
+
 export const readAll = async (inProgress: string | undefined) => {
   const matches = await inProgress === undefined
     ? Match.findAll({
@@ -24,7 +26,7 @@ export const createOne = async (matchData: Match) => {
   const { homeTeam: homeId, awayTeam: awayId } = matchData;
   if (homeId < 1 || homeId > 16) throw new HttpException(400, 'Team not found');
   if (awayId < 1 || awayId > 16) throw new HttpException(400, 'Team not found');
-  if (homeId === awayId) throw new HttpException(400, 'The teams must be different');
+  if (homeId === awayId) throw new HttpException(401, EQUAL_TEAM_ERROR);
   await Match.create(matchData);
   const options = matchData as unknown as WhereOptions<Match> | undefined;
   const matches = await Match.findAll({ where: options });
