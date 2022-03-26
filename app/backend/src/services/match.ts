233aa1d8dc91bfe78a66aa6a072be91e35900fbc
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-26 17:09:48 -0300

    feat: new service to insert a match on the database

--- app/backend/src/services/match.ts
+++ app/backend/src/services/match.ts
@@ -1,4 +1,6 @@
+import { WhereOptions } from 'sequelize';
 import { Match, Club } from '../database/models';
+import HttpException from '../classes/httpException';
 
 export const readAll = async (inProgress: string | undefined) => {
   const matches = await inProgress === undefined
@@ -18,4 +20,15 @@ export const readAll = async (inProgress: string | undefined) => {
   return matches;
 };
 
-export default readAll;
+export const createOne = async (matchData: Match) => {
+  const { homeTeam: homeId, awayTeam: awayId } = matchData;
+  if (homeId < 1 || homeId > 16) throw new HttpException(400, 'Team not found');
+  if (awayId < 1 || awayId > 16) throw new HttpException(400, 'Team not found');
+  if (homeId === awayId) throw new HttpException(400, 'The teams must be different');
+  await Match.create(matchData);
+  const options = matchData as unknown as WhereOptions<Match> | undefined;
+  const matches = await Match.findAll({ where: options });
+  const index = matches.length - 1;
+  const match = matches[index];
+  return match;
+};
