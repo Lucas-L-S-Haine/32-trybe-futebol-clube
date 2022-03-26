b7a67dfa6b32debd265abfbda806d2221efac0f5
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-26 19:43:49 -0300

    feat: new route to finish a match in progress

--- app/backend/src/routes/match.ts
+++ app/backend/src/routes/match.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { readAll, createOne } from '../controllers/match';
+import { readAll, createOne, finish } from '../controllers/match';
 import { validateToken } from '../auth';
 
 const matchRouter = Router();
@@ -8,5 +8,9 @@ matchRouter
   .route('/')
   .get(readAll)
   .post(validateToken, createOne);
+
+matchRouter
+  .route('/:id/finish')
+  .patch(finish);
 
 export default matchRouter;
