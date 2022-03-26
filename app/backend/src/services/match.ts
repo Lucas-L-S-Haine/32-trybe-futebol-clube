b7d785c61ef5b27c55f23abc4d885dcf983b488d
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-26 09:29:20 -0300

    refactor: add club names

--- app/backend/src/services/match.ts
+++ app/backend/src/services/match.ts
@@ -1,9 +1,20 @@
-import { Match } from '../database/models';
+import { Match, Club } from '../database/models';
 
 export const readAll = async (inProgress: string | undefined) => {
   const matches = await inProgress === undefined
-    ? Match.findAll()
-    : Match.findAll({ where: { inProgress: JSON.parse(inProgress as string) } });
+    ? Match.findAll({
+      include: [
+        { model: Club, as: 'homeClub', attributes: ['clubName'] },
+        { model: Club, as: 'awayClub', attributes: ['clubName'] },
+      ],
+    })
+    : Match.findAll({
+      where: { inProgress: JSON.parse(inProgress as string) },
+      include: [
+        { model: Club, as: 'homeClub', attributes: ['clubName'] },
+        { model: Club, as: 'awayClub', attributes: ['clubName'] },
+      ],
+    });
   return matches;
 };
 
