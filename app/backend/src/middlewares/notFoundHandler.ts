0314ba52aeafb185463e8eb68c8f664107e5e08c
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-26 16:08:32 -0300

    refactor: give a standard message for every request

--- app/backend/src/middlewares/notFoundHandler.ts
+++ app/backend/src/middlewares/notFoundHandler.ts
@@ -1,11 +1,8 @@
 import { RequestHandler } from 'express';
 
 const notFoundHandler: RequestHandler = (req, res, _next) => {
-  const entityName = req.originalUrl.slice(1);
-  const entity = entityName.slice(0, 1).toUpperCase()
-    .concat(entityName.slice(1).toLowerCase());
-  const error = `${entity} not found`;
-  return res.status(404).send({ error });
+  const message = 'Resource not found';
+  return res.status(404).send({ message });
 };
 
 export default notFoundHandler;
