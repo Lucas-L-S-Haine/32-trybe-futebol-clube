274e66c60f4b0b69d8e9d6cb62c72b41855f7e5b
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-26 19:44:30 -0300

    feat: new controller to finish a match in progress

--- app/backend/src/controllers/match.ts
+++ app/backend/src/controllers/match.ts
@@ -10,3 +10,8 @@ export const createOne: RequestHandler = (req, res, next) => service
   .createOne(req.body)
   .then((match) => res.status(201).json(match))
   .catch(next);
+
+export const finish: RequestHandler = (req, res, next) => service
+  .finish(Number(req.params.id))
+  .then(() => res.status(200).send({ message: 'OK' }))
+  .catch(next);
