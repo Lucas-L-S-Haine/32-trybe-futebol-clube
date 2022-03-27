701e0688b6fe84131eef068e75447bb063649184
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-27 14:13:17 -0300

    refactor: remove unused functions

--- app/backend/src/auth/bcryptjs.ts
+++
@@ -1,5 +0,0 @@
-import { genSaltSync, hashSync, compareSync } from 'bcryptjs';
-
-export const createSalt = (num: number): string => genSaltSync(num);
-export const createHash = (password: string, salt: string): string => hashSync(password, salt);
-export const validatePassword = (password: string, hash: string) => compareSync(password, hash);
--- app/backend/src/auth/index.ts
+++ app/backend/src/auth/index.ts
@@ -1,11 +1,7 @@
 import { createToken, readToken, validateToken } from './jwt';
-import { createSalt, createHash, validatePassword } from './bcryptjs';
 
 export {
   createToken,
   readToken,
   validateToken,
-  createSalt,
-  createHash,
-  validatePassword,
 };
