da6317e2070576ffed8e19ac63cda241d8e62d52
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-27 02:54:10 -0300

    refactor: bring associations to models index file

--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -7,6 +7,11 @@ import Match from './Match';
 const databaseConfig = databaseOptions as unknown as Options;
 const sequelize = new Sequelize(databaseConfig);
 
+Club.hasMany(Match, { foreignKey: 'id', as: 'homeTeam' });
+Club.hasMany(Match, { foreignKey: 'id', as: 'awayTeam' });
+Match.belongsTo(Club, { foreignKey: 'homeTeam', as: 'homeClub' });
+Match.belongsTo(Club, { foreignKey: 'awayTeam', as: 'awayClub' });
+
 export {
   Sequelize,
   sequelize,
