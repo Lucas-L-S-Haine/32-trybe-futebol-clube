781bdb807ada959e47359852847a849cf43b466c
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-28 11:53:03 -0300

    feat: new interface

--- app/backend/src/utils/interfaces.ts
+++ app/backend/src/utils/interfaces.ts
@@ -8,3 +8,16 @@ export interface ILogin {
   email: string;
   password: string;
 }
+
+export interface IScore {
+  name: string | number;
+  totalPoints: string | number;
+  totalGames: string | number;
+  totalVictories: string | number;
+  totalDraws: string | number;
+  totalLosses: string | number;
+  goalsFavor: string | number;
+  goalsOwn: string | number;
+  goalsBalance: string | number;
+  efficiency: string | number;
+}
