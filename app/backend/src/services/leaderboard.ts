699174e4c431e0bccbd01a1ce284b210fcea4c80
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-05-27 14:30:12 -0300

    style: erase comments

--- app/backend/src/services/leaderboard.ts
+++ app/backend/src/services/leaderboard.ts
@@ -35,30 +35,3 @@ export const readAway = async () => {
     efficiency: Number(team.efficiency),
   }));
 };
-
-/*
-Classificação: Posição na classificação;
-Time: Nome do time;
-P: Total de Pontos;
-J: Total de Jogos;
-V: Total de Vitórias;
-E: Total de Empates;
-D: Total de Derrotas;
-GP: Gols marcados a favor;
-GC: Gols marcados contra;
-SG: Saldo total de gols;
-%: Aproveitamento do time.
-*/
-
-/*
-name: 'Santos',
-totalPoints: '9',
-totalGames: '3',
-totalVictories: '3',
-totalDraws: '0',
-totalLosses: '0',
-goalsFavor: '9',
-goalsOwn: '3',
-goalsBalance: '6',
-efficiency: '100'
-*/
